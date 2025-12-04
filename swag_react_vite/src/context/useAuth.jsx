e4acab6cd4313e1e436a6fcfe03deab7e3e621aa
Syfer <valentinmonnier@MacBook-Air-de-Syfer.local>
2025-12-04 17:08:30 +0100

    Dripped as fuck

--- swag_react_vite/src/context/useAuth.jsx
+++ swag_react_vite/src/context/useAuth.jsx
@@ -1,21 +1,31 @@
 import { createContext, useContext, useState, useEffect } from "react";
+{/*CreateContext = Espace de Stockage
+   useContext = Utiliser l'espace de stockage
+   useState = Stocke l'utilisateur connecté
+   useEffect = Exécute le code au lancement de l'app*/}
 import {
     getCurrentUser,
     loginUser,
     registerUser
 } from "../components/utils/AuthMock";
 
+{/*Auth contexte crée un espace de stockage null par défaut*/}
 const AuthContext = createContext(null);
 
 export function AuthProvider({ children }) {
     const [user, setUser] = useState(null);
+    {/*user contient l'user connecter
+       setUser met à jour cet utilisateur*/}
 
     useEffect(() => {
         setUser(getCurrentUser());
+        {/*getCurrentUser()) va chercher l'utilisateur dans l'espace de stockage*/}
     }, []);
 
     const login = (email, password) => {
         const res = loginUser({ email, password });
+        {/*Appelle le Login de l'API Fake Mock
+            S'il réussit -> on sauvegarde l'user dans le state AuthContext*/}
         if (res.success) {
             setUser(res.user);
         }
@@ -32,11 +42,14 @@ export function AuthProvider({ children }) {
 
     const logout = () => {
         localStorage.removeItem("swag_current_user");
+        {/*Suppression de l'user en stockage local
+            Met user à null -> logout*/}
         setUser(null);
     };
 
     return (
         <AuthContext.Provider value={{ user, login, register, logout }}>
+            {/*Donne l'accès à user, login, register, logout sans props */}
             {children}
         </AuthContext.Provider>
     );
